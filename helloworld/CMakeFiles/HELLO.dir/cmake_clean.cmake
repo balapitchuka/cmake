afae6af37c9ee6b228ee7a2a357a0a60f7fe56ef
Balasubrahmanyam <pbs@gmail.com>
2019-11-01 19:44:11 +0530

    cmake basic scripting and check os example implementation

---
+++ helloworld/CMakeFiles/HELLO.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HELLO.dir/helloworld.cpp.obj"
+  "HELLO.pdb"
+  "HELLO.exe"
+  "HELLO.exe.manifest"
+  "libHELLO.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HELLO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
